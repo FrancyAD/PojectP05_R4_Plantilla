7d79ade5d22f2b82b34a82c73ce3731af96534d0
FrancyAD <faalvaradod@unal.edu.co>
2021-08-15 22:49:02 -0500

    Limpieza de código

--- src/main/java/util/JDBCUtilities.java
+++ src/main/java/util/JDBCUtilities.java
@@ -8,8 +8,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCUtilities {
-    // private static final String DATABASE_LOCATION = "ProyectosConstruccion.db";
-    private static final String DATABASE_LOCATION = "C:\\Users\\Asus\\Documents\\Mis Documentos\\Documentos\\Academia\\Programacion\\MisionTic\\Ciclo 2\\Retos\\Reto_U4\\PojectP05_R4_Plantilla\\ProyectosConstruccion.db";
+    private static final String DATABASE_LOCATION = "ProyectosConstruccion.db";    
 
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:sqlite:" + DATABASE_LOCATION;
